b4a17914db4247f40ab6f8d1400e4c4590dd291f
yabenatti <min.benatti@gmail.com>
2017-08-07 17:30:19 -0300

    Implementing resizable label

--- TextureChat/ViewController.swift
+++ TextureChat/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import AsyncDisplayKit
 
-class ViewController: ASViewController<ASDisplayNode>, ASTableDataSource, ASTableDelegate {
+class ViewController: ASViewController<ASDisplayNode> {
+
+    var strings: [String]!
 
     var tableNode: ASTableNode {
         return node as! ASTableNode
@@ -19,6 +21,8 @@ class ViewController: ASViewController<ASDisplayNode>, ASTableDataSource, ASTabl
         super.init(node: ASTableNode())
         tableNode.delegate = self
         tableNode.dataSource = self
+        
+        strings = ["one", "two", "three", "four", "five"]
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -35,23 +39,75 @@ class ViewController: ASViewController<ASDisplayNode>, ASTableDataSource, ASTabl
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: ASTableNode data source and delegate.
-    
-    func tableNode(_ tableNode: ASTableNode, nodeForRowAt indexPath: IndexPath) -> ASCellNode {
-        let node = ASTextCellNode()
-        node.text = String(format: "[%ld.%ld] says hello!", indexPath.section, indexPath.row)
-        
-        return node
+}
+
+// MARK: - ASTableDelegate
+
+extension ViewController: ASTableDelegate {
+    func tableNode(_ tableNode: ASTableNode, willBeginBatchFetchWith context: ASBatchContext) {
+        nextPageWithCompletion { (results) in
+            self.insertNewRows(results)
+            context.completeBatchFetching(true)
+        }
     }
     
-    func numberOfSections(in tableNode: ASTableNode) -> Int {
-        return 1
+    func shouldBatchFetch(for tableNode: ASTableNode) -> Bool {
+        return true
     }
-    
+}
+
+// MARK: - ASTableDataSource
+
+extension ViewController: ASTableDataSource {
     func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return strings.count
     }
+    
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        
+        return {
+            let node = ChatMessageCell()
+            if indexPath.row == 1 {
+                node.descriptionTextNode.attributedText = NSAttributedString(string:"small text")
+            }
+            return node
+        }
+    }
+    
+    @objc(tableNode:constrainedSizeForRowAtIndexPath:)
+    func tableNode(_ tableNode: ASTableNode, constrainedSizeForRowAt indexPath: IndexPath) -> ASSizeRange {
+        let min = CGSize(width: UIScreen.main.bounds.size.width, height: 16)
+        let max = CGSize(width: UIScreen.main.bounds.size.width, height: CGFloat.greatestFiniteMagnitude)
+        return ASSizeRange(min: min, max: max)
+    }
+}
 
+// MARK: - Helpers
 
+extension ViewController {
+    func nextPageWithCompletion(_ block: @escaping (_ results: [String]) -> ()) {
+        let moreAnimals = Array(self.strings[0 ..< 5])
+        
+        DispatchQueue.main.async {
+            block(moreAnimals)
+        }
+    }
+    
+    func insertNewRows(_ newStrings: [String]) {
+        let section = 0
+        var indexPaths = [IndexPath]()
+        
+        let newTotalNumberOfPhotos = strings.count + newStrings.count
+        
+        for row in strings.count ..< newTotalNumberOfPhotos {
+            let path = IndexPath(row: row, section: section)
+            indexPaths.append(path)
+        }
+        
+        strings.append(contentsOf: newStrings)
+        if let tableNode = node as? ASTableNode {
+            tableNode.insertRows(at: indexPaths, with: .none)
+        }
+    }
 }
 
